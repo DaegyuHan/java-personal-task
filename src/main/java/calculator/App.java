fba9747ff7e1eccb5a03be3967601f74b4dfc2c1
Daegyu Han <bigstar77han@gmail.com>
2024-07-24 19:54:01 +0900

    연산 결과 10개를 저장할 수 있는 배열 선언,생성 및 결과 저장

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,5 @@
 package calculator;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -6,6 +7,14 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int restart = 1;
+        int[] resultArr = new int[10];
+        int count = 0;
+
+        while (restart ==1) {
+
+        int result = 0; //result 선언 및 초기화
+        System.out.println("------------------------");
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
         System.out.println("두 번째 숫자를 입력하세요: ");
@@ -13,28 +22,50 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char oper = sc.next().charAt(0);
 
-        int result = 0; //result 선언 및 초기화
 
-        if (oper == '+') {
-            result = num1 + num2;
-        } else if (oper == '-') {
-            if (num1 >= num2) {
+            if (oper == '+') {   // 덧셈 연산
+                result = num1 + num2;
+                System.out.println("결과 : " + result);
+                System.out.println("------------------------");
+            } else if (oper == '-') {   // 뺄셈 연산
                 result = num1 - num2;
+                System.out.println("결과 : " + result);
+                System.out.println("------------------------");
+            } else if (oper == '*') {   // 곱셈 연산
+                result = num1 * num2;
+                System.out.println("결과 : " + result);
+                System.out.println("------------------------");
+            } else if (oper == '/') {   // 나눗셈 연산
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("결과 : " + result);
+                    System.out.println("------------------------");
+                }
             } else {
-                result = num2 - num1;
+                System.out.println("정확한 값을 입력해주세요.");
             }
-        } else if (oper == '*') {
-            result = num1 * num2;
-        } else if (oper == '/') {
-            if (num2 == 0 ) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-            } else {
-                result = num1 / num2;
-            }
-        } else {
-            System.out.println("정확한 값을 입력해주세요.");
-        }
 
-        System.out.println("결과 : " + result);
+            resultArr[count] = result;
+            System.out.println(Arrays.toString(resultArr));
+            count++;
+
+
+            int ask_replay = 0;     // 다시 계산하기 위한 반복문 제어용 변수 선언,초기화
+
+            while (ask_replay == 0) {
+                System.out.println("더 계산하시겠습니까? (숫자 입력)" + "\n" + "1.yes" + "\n" + "2.no");
+                ask_replay = sc.nextInt();
+                if (ask_replay == 1) {
+                    break;
+                } else if (ask_replay == 2) {
+                    restart = 2;
+                } else {
+                    System.out.println("정확한 값을 입력해주세요.");
+                    ask_replay = 0;
+                }
+            }
+        }
     }
 }
