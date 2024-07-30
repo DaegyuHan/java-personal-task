48aff3b8cc24c2ef5341f79aca142e7b782e7b7b
Daegyu Han <bigstar77han@gmail.com>
2024-07-31 04:36:28 +0900

    2-9 ArithmeticCalcultor í 클래스 연산 메서드 책임 분산

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -2,22 +2,34 @@ package calculator;
 
 public class ArithmeticCalculator extends Calculator {
 
+    AddOperator addOperator;
+    SubtractOperator subtractOperator;
+    MultiplyOperator multiplyOperator;
+    DivideOperator divideOperator;
+
+    public ArithmeticCalculator() {
+        addOperator = new AddOperator();
+        subtractOperator = new SubtractOperator();
+        multiplyOperator = new MultiplyOperator();
+        divideOperator = new DivideOperator();
+    }
+
     public int calculate(int num1, int num2, char operation) throws MessageException {
 
         int result = 0;
         switch (operation) {
             case '+':
-                result = num1 + num2;
+                result = addOperator.addOper(num1, num2);
                 break;
             case '-':
-                result = num1 - num2;
+                result = subtractOperator.subtractOper(num1, num2);
                 break;
             case '*':
-                result = num1 * num2;
+                result = multiplyOperator.multiplyOper(num1, num2);
                 break;
             case '/':
                 if (num2 != 0) {
-                    result = num1 / num2;
+                    result = divideOperator.divideOper(num1, num2);
                     break;
                 } else {
                     throw new MessageException("분모는 0 이 되면 안됩니다.");
---
+++ src/main/java/calculator/DivideOperator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class DivideOperator {
+    public int divideOper(int num1, int num2) {
+        int result = num1 / num2;
+        return result;
+    }
+}
