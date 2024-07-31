95456ad82344210e1f3b3ebfa4f7c63cfb8a9cad
Daegyu Han <bigstar77han@gmail.com>
2024-07-31 15:40:55 +0900

    개인 과제 충돌 ì 발견 후 수정 (최종)

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,7 +16,7 @@ public class App {
         int restart = 1;                                    // 계산기 반복문 접근 위한 변수
         int checking = 1;
 
-<<<<<<< HEAD
+
         while (restart == 1) {
             System.out.println("------------------------");
             System.out.println("""
@@ -42,37 +42,7 @@ public class App {
                     } catch (MessageException e) {
                         System.out.println(e.getMessage());
                     }
-=======
-        int result = 0; //result 선언 및 초기화
-        System.out.println("------------------------");
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        char operation = sc.next().charAt(0);
 
-
-            if (operation == '+') {   // 덧셈 연산
-                result = num1 + num2;
-                System.out.println("결과 : " + result);
-                System.out.println("------------------------");
-            } else if (operation == '-') {   // 뺄셈 연산
-                result = num1 - num2;
-                System.out.println("결과 : " + result);
-                System.out.println("------------------------");
-            } else if (operation == '*') {   // 곱셈 연산
-                result = num1 * num2;
-                System.out.println("결과 : " + result);
-                System.out.println("------------------------");
-            } else if (operation == '/') {   // 나눗셈 연산
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                } else {
-                    result = num1 / num2;
-                    System.out.println("결과 : " + result);
-                    System.out.println("------------------------");
->>>>>>> a8f490ef969413291c06e2ad107187b1af1b63bd
                 }
 
 
@@ -113,7 +83,7 @@ public class App {
                 calculator.inquiryPieResult();
             }
 
-<<<<<<< HEAD
+
                 int askReplay = 0;     // 다시 계산하기 위한 반복문 제어용 변수 선언,초기화
 
                 while (askReplay == 0) {
@@ -131,28 +101,7 @@ public class App {
                         System.out.println("정확한 값을 입력해주세요.");
                         askReplay = 0;
                     }
-=======
-            resultArr[count] = result;          // 0 으로 초기화된 count 값부터 시작해서 resultArr 배열에 result 값 저장
-            System.out.println(Arrays.toString(resultArr));
-            count++;
 
-
-            int ask_replay = 0;     // 다시 계산하기 위한 반복문 제어용 변수 선언,초기화
-
-            while (ask_replay == 0) {
-                System.out.println("""
-                        더 계산하시겠습니까? (숫자 입력)
-                        1.yes
-                        2.no""");
-                ask_replay = sc.nextInt();
-                if (ask_replay == 1) {
-                    break;
-                } else if (ask_replay == 2) {
-                    restart = 2;
-                } else {
-                    System.out.println("정확한 값을 입력해주세요.");
-                    ask_replay = 0;
->>>>>>> a8f490ef969413291c06e2ad107187b1af1b63bd
                 }
 
         }
